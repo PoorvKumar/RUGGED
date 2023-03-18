8c3df973056f6ff23cd5e0bc601fc013fff1bc19
Divyank <divyankkhajuria88@gmail.com>
2023-03-19 00:45:22 +0530

    graph added

---
+++ js/selldash.js
@@ -0,0 +1,39 @@
+const ctx = document.getElementById('myChart');
+const data = {
+    labels: [
+      'Red',
+      'Blue',
+      'Yellow'
+    ],
+    datasets: [{
+      label: 'My First Dataset',
+      data: [300, 50, 100],
+      backgroundColor: [
+        'rgb(255, 99, 132)',
+        'rgb(54, 162, 235)',
+        'rgb(255, 205, 86)'
+      ],
+      hoverOffset: 4
+    }]
+  };
+  new Chart(ctx, {
+    type: 'pie',
+    data: data});
+const ctx2 = document.getElementById('myChart2');
+const labels = Utils.months({count: 7});
+const data1 = {
+  labels: labels,
+  datasets: [{
+    label: 'My First Dataset',
+    data: [65, 59, 80, 81, 56, 55, 40],
+    fill: false,
+    borderColor: 'rgb(75, 192, 192)',
+    tension: 0.1
+  }]
+};
+new Chart(ctx2,{
+    type: 'pie',
+    data: data1 
+});
+
+
