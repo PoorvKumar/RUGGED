2dc36f07cbd3d19566fd6e6fc248a18aefcf62b2
Divyank <divyankkhajuria88@gmail.com>
2023-03-20 02:37:58 +0530

    seller Dashboard changes

--- app.js
+++ app.js
@@ -3,16 +3,17 @@ var path = require('path');
 var ejs=require('ejs')
 
 var app = express();
-
+const sellerdashboardRoutes=require('./routes/sellerDashboardRoutes')
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', (req, res, next) => {
-  res.render('productSearchPage.ejs');
-})
+// app.use('/', (req, res, next) => {
+//   res.render('productSearchPage.ejs');
+// })
+app.use(sellerdashboardRoutes)
 // app.use('/',(req,response)=>
 // {
 //   response.render('LandingPage/product-card-slider.ejs');
