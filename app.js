db9af3e12bb1f8ab774b869a2aca133dc02451fc
Divyank <divyankkhajuria88@gmail.com>
2023-03-21 21:50:44 +0530

    appjs

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ var ejs=require('ejs')
 var app = express();
 const sellerdashboardRoutes=require('./routes/sellerDashboardRoutes')
 const influencerBlogRoutes=require('./routes/influencerBlogPageRoutes')
+const wishlistroute=require('./routes/wishlistRoutes')
+const productPageRoutes=require('./routes/productPage')
+const productSearchPage=require('./routes/productSearchpage')
+const returnsOrderPage=require('./routes/returnsAndorder')
+const dashboardUserPage=require('./routes/dashboardUser')
 const index=require('./routes/index.js')
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,6 +23,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(index);
 app.use(influencerBlogRoutes)
 app.use(sellerdashboardRoutes)
+app.use(wishlistroute)
+app.use(productPageRoutes)
+app.use(productSearchPage)
+app.use(returnsOrderPage)
+app.use(dashboardUserPage)
 // app.use('/',(req,response)=> 
 // {
 //   response.render('LandingPage/product-card-slider.ejs');
