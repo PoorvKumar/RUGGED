18308202a93707f6ba676c87a946677be213afdf
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2023-03-22 10:13:31 +0530

    app.js formatted

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
-var express = require('express');
-var path = require('path');
-var ejs=require('ejs');
-const bodyparser=require('body-parser')
-const session=require('express-session')
-var app = express();
-var sqlite3 = require('sqlite3').verbose()
-const dbh=new sqlite3.Database('./database/project.db')
+const express = require('express');
+const app = express();
+const path = require('path');
+const ejs=require('ejs');
+
+const bodyparser=require('body-parser'); //body-parser
+const session=require('express-session'); //express-session
+
+const sqlite3 = require('sqlite3').verbose(); //sqlite3
+
+// const dbh=new sqlite3.Database('./database/project.db');
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+// app.use(bodyparser.json)
+app.use(bodyparser.urlencoded({extended:true}));
+
+//routing
 const sellerdashboardRoutes=require('./routes/sellerDashboardRoutes');
 const influencerBlogRoutes=require('./routes/blogPost');
 const wishlistroute=require('./routes/wishlist');
@@ -15,24 +27,28 @@ const returnsOrderPage=require('./routes/returnsAndorder');
 const dashboardUserPage=require('./routes/dashboardUser');
 const sellerPortal=require('./routes/sellerPortal');
 const index=require('./routes/index.js');
-const login=require('./routes/loginroute')
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname, 'public')));
-// app.use(bodyparser.json)
-app.use(bodyparser.urlencoded({extended:true}))
+const login=require('./routes/login');
+
 
 // app.use('/index', (req, res, next) => {
 //   res.render('index.ejs');
 // })
 //==============================================================
-let db = new sqlite3.Database('./database/project.db')
+let db = new sqlite3.Database('./database/project.db', (err) => { //Initialise Connection with in-memory DB
+    if (err) {
+        // Cannot open database
+        console.error(err.message)
+        throw err
+    } else {
+        console.log('Connected to the SQLite database.')
+    }
+});
+
 app.use(session({
     secret:'secret',
     resave:false,
     saveUninitialized:true
-}))
+}));
 app.post('/login',(req,res)=>{
     let user = [];
         Email=req.body.email
@@ -53,8 +69,14 @@ app.post('/login',(req,res)=>{
     else{
         res.end("Invalid")
     }
-})
-})
+});
+});
+db.each('select * from customer',(err,rows)=>{
+    console.log(rows);
+});
+
+//using routes
+app.use(index);
 app.use(influencerBlogRoutes);
 app.use(sellerdashboardRoutes);
 app.use(dashboardUserPage);
@@ -63,7 +85,6 @@ app.use(productPageRoutes);
 app.use(productSearchPage);
 app.use(returnsOrderPage);
 app.use(wishlistroute);
-app.use(login)
-app.use(index)
+app.use(login);
 
-app.listen(3010)
+app.listen(3000);
