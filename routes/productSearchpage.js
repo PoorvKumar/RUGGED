e686afa0c1f033aba4c2e881d62c030c158382fd
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2023-03-22 12:29:57 +0530

    product search page ejs title error solved

--- routes/productSearchpage.js
+++ routes/productSearchpage.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/productsearch', function(req, res, next) {
   
-  res.render('productSearchPage.ejs', { title: '' },{user:user});
+  res.render('productSearchPage.ejs', { title: '' });
 });
 
 module.exports = router;
