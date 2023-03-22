cdd4fa8fd6bf72a1cfcad2a5883ab9bbc3ff8691
Akash <akashkulkarni4444@gmail.com>
2023-03-22 16:01:48 +0530

    login invalid credentials fixed

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const ejs=require('ejs');
+const ejs = require('ejs');
 
-const bodyparser=require('body-parser'); //body-parser
-const session=require('express-session'); //express-session
+const bodyparser = require('body-parser'); //body-parser
+const session = require('express-session'); //express-session
 
 const sqlite3 = require('sqlite3').verbose(); //sqlite3
 
@@ -15,21 +15,21 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 // app.use(bodyparser.json)
-app.use(bodyparser.urlencoded({extended:true}));
+app.use(bodyparser.urlencoded({ extended: true }));
 
 //routing
-const sellerdashboardRoutes=require('./routes/sellerDashboardRoutes');
-const influencerBlogRoutes=require('./routes/blogPost');
-const wishlistroute=require('./routes/wishlist');
-const productPageRoutes=require('./routes/productPage');
-const productSearchPage=require('./routes/productSearchpage');
-const returnsOrderPage=require('./routes/returnsAndorder');
-const dashboardUserPage=require('./routes/dashboardUser');
-const sellerPortal=require('./routes/sellerPortal');
-const index=require('./routes/index.js');
-const login=require('./routes/login');
-const signup=require('./routes/signup');
-const logout=require('./routes/logout')
+const sellerdashboardRoutes = require('./routes/sellerDashboardRoutes');
+const influencerBlogRoutes = require('./routes/blogPost');
+const wishlistroute = require('./routes/wishlist');
+const productPageRoutes = require('./routes/productPage');
+const productSearchPage = require('./routes/productSearchpage');
+const returnsOrderPage = require('./routes/returnsAndorder');
+const dashboardUserPage = require('./routes/dashboardUser');
+const sellerPortal = require('./routes/sellerPortal');
+const index = require('./routes/index.js');
+const login = require('./routes/login');
+const signup = require('./routes/signup');
+const logout = require('./routes/logout');
 // app.use('/index', (req, res, next) => {
 //   res.render('index.ejs');
 // })
@@ -45,46 +45,77 @@ let db = new sqlite3.Database('./database/project.db', (err) => { //Initialise C
 });
 
 app.use(session({
-    secret:'secret',
-    resave:false,
-    saveUninitialized:true
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: true
 }));
-app.post('/login',(req,res)=>{
-    let user = [];
-        email=req.body.email;
-        // email.toLowerCase();
-        var sql = "SELECT * FROM Customer WHERE email = ?";
-        db.all(sql, email, function(err, rows) {
-            if (err){
-                res.status(400).json({"error": err.message})
-                return;
+// app.post('/login',(req,res)=>{
+//     let user = [];
+//         var email=req.body.email;
+//         // email.toLowerCase();
+//         var sql = "SELECT * FROM Customer WHERE email = ?";
+//         db.all(sql, email, function(err, rows) {
+//             if (err){
+//                 res.status(400).json({"error": err.message})
+//                 return;
+//             }
+
+//             rows.forEach(function (row) {
+//                 user.push(row);                
+//             })
+//     if(req.body.email==user[0].email && req.body.password==user[0].password){
+//         // req.session.user=currentUser;
+//         req.session.user=user[0]
+//         res.redirect('/')
+//     }
+//     else{
+//         res.end("Invalid Username or Password");
+
+//     }
+// });
+// });
+
+app.post('/login', (req, res, next) => {
+    let sql = `SELECT * FROM customer WHERE email = "${req.body.email}" AND password = "${req.body.password}"`;
+    var x;
+
+    db.all(sql, (err, rows) => {
+        if (err) {
+            next(err);
+            return;
+        }
+        if (!rows) {
+            res.status(400);
+            res.send('Invalid username or password');
+            return
+        }
+        rows.forEach((row) => {
+            if (row.username === req.body.username && row.password === req.body.password) {
+                x = 1;
             }
+            else {
+                x = 2;
+                db.close();
+            }
+        })
+        if (x === 1) {
+            res.redirect('/');
+        }
+        else { res.render('login.ejs' ,{data:"Invalid login credentials.",title:"Login Portal"}); }
+    })
+})
 
-            rows.forEach(function (row) {
-                user.push(row);                
-            })
-    if(req.body.email==user[0].email && req.body.password==user[0].password){
-        // req.session.user=currentUser;
-        req.session.user=user[0]
-        res.redirect('/')
-    }
-    else{
-        res.end("Invalid Username or Password");
-    }
-});
-});
-
-//using routes
-app.use(index);
-app.use(influencerBlogRoutes);
-app.use(sellerdashboardRoutes);
-app.use(dashboardUserPage);
-app.use(sellerPortal);
-app.use(productPageRoutes);
-app.use(productSearchPage);
-app.use(returnsOrderPage);
-app.use(wishlistroute);
-app.use(login);
-app.use(signup);
-app.use(logout)
-app.listen(3000);
+    //using routes
+    app.use(index);
+    app.use(influencerBlogRoutes);
+    app.use(sellerdashboardRoutes);
+    app.use(dashboardUserPage);
+    app.use(sellerPortal);
+    app.use(productPageRoutes);
+    app.use(productSearchPage);
+    app.use(returnsOrderPage);
+    app.use(wishlistroute);
+    app.use(login);
+    app.use(signup);
+    app.use(logout)
+    app.listen(3000);
--- routes/login.js
+++ routes/login.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/login', function(req, res, next) {
-  res.render('login.ejs', { title: 'Login Portal' });
+  res.render('login.ejs', { data:'',title: 'Login Portal' });
 });
 
 module.exports = router;
