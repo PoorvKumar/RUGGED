4a3ceaf0365360068dd844bcf0a95321f8b17562
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2023-04-19 21:19:10 +0530

    app.js

--- routes/login.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET Login page. */
-router.get('/login', function(req, res, next) {
-  res.render('login.ejs', { data:'',title: 'Login Portal' });
-});
-
-module.exports = router;
--- routes/signup.js
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-const express = require('express');
-const router = express.Router();
-router.get('/signup', (req, res, next) => {
-    res.render('signup.ejs');
-  });
-module.exports = router;
