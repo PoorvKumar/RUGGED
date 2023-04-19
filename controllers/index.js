8d034bb61a06ecffc1ad5267cc4293a09b886e18
Divyank <divyankkhajuria88@gmail.com>
2023-04-19 20:52:18 +0530

    minor changes

--- controllers/index.js
+++ controllers/index.js
@@ -1,20 +1,42 @@
 const Product=require('../models/product')
+let products1,products2,products3;
 exports.getLandingpage=(req,res,next)=>{
-    Product.find()
-    .then(products => {
-      if(!req.session.isLoggedin){
-        res.render('index',{
-            pageTitle:'RUGGED',
-            isLoggedin:req.session.isLoggedin,
-            product:products
-        })}
-        else{
+    Product.find({categories:'Shoes'})
+    .then(product=>{
+        products1=product
+    Product.find({categories:'Shoes'})
+    .then(product=>{
+        products2=product
+    Product.find({categories:'Shoes'})
+    .then(product=>{
+        products3=product
+        if(!req.session.isLoggedin){
             res.render('index',{
                 pageTitle:'RUGGED',
                 isLoggedin:req.session.isLoggedin,
-                user:req.session.user,
-                product:products
-            })
-        }
-    });
+                prod:products2
+            })}
+            else{
+                res.render('index',{
+                    pageTitle:'RUGGED',
+                    isLoggedin:req.session.isLoggedin,
+                    user:req.session.user,
+                    prod:products3
+                })
+            }
+
+    })
+    .catch(err=>{
+        console.log(err)
+    })
+    })
+    .catch(err=>{
+        console.log(err)
+    })
+    })
+    .catch(err=>{
+        console.log(err)
+    })
+
+      
 }
--- models/user.js
+++ models/user.js
@@ -23,7 +23,25 @@ const user=new Schema({
     },
     address:{
         type:String
+    },
+    cart:{
+        item:[
+            {
+                productID: {type: Schema.Types.ObjectId, ref:'Product',required:true},
+                quantity: {type: Number, required:true}
+            }
+        ]
     }
 });
+user.methods.addToCart=(product)=>{
+    //Checking if product is already in the cart
+    const productidx=this.cart.find(cartProduct => cartProduct.productID.toString()===product._id.toString())
+    const updatedCart=[...this.cart.item] //Making shallow copy (one object two different instance pointing to it)
+    if(productidx<0){
+      updatedCart.push({productID:product._id,quantity:1})
+    }  
+    this.cart={item:updatedCart}
+    return this.save()
+}
 
 module.exports=mongoose.model('User',user);
