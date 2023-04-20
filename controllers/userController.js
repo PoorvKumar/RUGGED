cea7b9615c710c69210d7db7b25e660076b2326a
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2023-04-20 18:47:46 +0530

    update working but only by clicking save

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 const Product=require('../models/product');
+const User=require("../models/user");
 
 exports.getwishList=(req,res,next)=>{
     Product.find()
@@ -14,8 +15,36 @@ exports.getwishList=(req,res,next)=>{
 
 exports.getUserDashboard=(req,res)=>
 {
-    const userId=req.params.id;
-    console.log(userId);
+    // const userId=req.params.id;
+    // console.log(userId);
     res.render('userDashboard',{ pgTTL:'User DashBoard', user:req.session.user, isLoggedin: req.session.isLoggedin });
 }
 
+exports.updateUserPost=(req,res)=>
+{
+    const userId=req.query.id;
+    // const updatedUserData=req.body;
+    const updatedUserData=
+    {
+        firstname:req.body.First,
+        lastname:req.body.Last,
+        phoneno:req.body.Phone,
+        email:req.body.email,
+        address:req.body.Address,
+        adddressAlt:req.body.AddressAlt,
+        state:req.body.state,
+        country:req.body.country,
+    }
+    console.log(updatedUserData);
+
+    User.findByIdAndUpdate(userId,updatedUserData,{ new:true })
+    .then(updatedUser=>
+        {
+            res.render('userDashboard',{ pgTTL:'User DashBoard', user:updatedUser, isLoggedin: req.session.isLoggedin });
+        })
+    .catch(err=>
+        {
+            console.error(err);
+            res.status(500).send("EError Updating user");
+        });
+}
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,6 +6,7 @@ const router=express.Router();
 
 router.get('/wishList',userController.getwishList);
 router.get('/userDashboard',authMiddleware,userController.getUserDashboard);
+router.post('/userDashboard',userController.updateUserPost);
 
 
 module.exports=router
