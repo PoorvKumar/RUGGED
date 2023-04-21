99cac624c943d95a8b8f09175c807386b14deed4
Akash <akashkulkarni4444@gmail.com>
2023-04-21 13:59:13 +0530

    added review.js. Some changes in product.js . filters array added in filterRoute.js

--- models/product.js
+++ models/product.js
@@ -1,70 +1,80 @@
-const mongoose=require('mongoose')
-const Schema=mongoose.Schema
-const product=new Schema({
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const product = new Schema({
   // _id:{
   //   type:String
   // },
-  name:{
-    type:String,
-    required:true
+  name: {
+    type: String,
+    required: true
   },
-  price:{
-    type:Number,
-    required:true
+  price: {
+    type: Number,
+    required: true
   },
-  description:{
-    type:String,
-    required:true
+  description: {
+    type: String,
+    required: true
   },
-  company:{
-    type:String,
-    required:true
+  // company:{
+  //   type:String,
+  //   required:true
+  // },
+  discount: {
+    type: Number,
+    required: true
   },
-  discount:{
-    type:Number,
-    required:true
+  dimension: {
+    // should have length,breadth and height
+    type: String,
+    required: true
   },
-  dimension:{
-    type:String,
-    required:true
+  weight: {
+    type: String,
+    required: true
   },
-  weight:{
-    type:String,
-    required:true
+  colors: {
+    type: String,
+    required: true
   },
-  colors:{
-    type:String,
-    required:true
+  brand: {
+    type: String,
+    required: true
   },
-  brand:{
-    type:String,
-    required:true
+  // only one tags or categories should be present
+  tags: {
+    type: String,
+    required: true
   },
-  tags:{
-    type:String,
-    required:true
+  categories: {
+    type: String,
+    required: true
   },
-  categories:{
-    type:String,
-    required:true
+  // photos should be an array of image urls
+  photosURLS: [{
+    photoURL: {
+      type: String
+    }
+  }],
+  table: {
+    type: String,
+    required: true
   },
-  photos:{
-    type:String
+  // rather than review id we should have entire review document here.
+  // it should consist of no. of 5 star, 4 star, 3 star , 2 star , 1 star, and 0 star reviews. 
+  // it should also consist of average no. of stars as review.
+  // it should also have an array of documents consisting of individual reviews.
+  reviewsID: {
+    type: String
   },
-  table:{
-    type:String,
-    required:true
+  quantity: {
+    type: Number,
+    required: true
   },
-  reviewID:{
-    type:String
+  sellerID: {
+    type: String
   },
-  quantity:{
-    type:Number,
-    required:true
-  },
-  sellerID:{
-    type:String
-  },
+  
 });
 
-module.exports=mongoose.model('Product',product);
+module.exports = mongoose.model('Product', product);
--- routes/filterRoute.js
+++ routes/filterRoute.js
@@ -1,7 +1,8 @@
-const express=require('express');
-const router=express.Router();
+const express = require('express');
+const router = express.Router();
 
-const Product=require("../models/product");
+const Product = require("../models/product");
+const product = require('../models/product');
 
 router.get('/filterCustomerRating', (req, res) => {
     // using search term and passing it ahead
@@ -15,7 +16,7 @@ router.get('/filterCustomerRating', (req, res) => {
     const offerSelected = req.query.offerSelected;
     // price
     const priceLL = req.query.pricefrom;
-    const priceUL =req.query.priceto;
+    const priceUL = req.query.priceto;
     // influencers choice
     const ic = req.query.InfluencersChoice;
     // rugged verrified
@@ -24,20 +25,69 @@ router.get('/filterCustomerRating', (req, res) => {
     const clr = req.query.colour;
     // available
     const avl = req.query.availability;
-
+    console.log(avl.toString());
 
     Product.find({ name: { $regex: searchTerm, $options: 'i' }, })
         .then(products => {
             // res.json(products);
-            console.log(products);
-            res.render('productSearchPage.ejs',{productsData:products, user:req.session.user,isLoggedin:req.session.isLoggedin,searchTerm:searchTerm});
+            // console.log(products);
+            // products.filter({price:{$gt:priceLL,$lt:priceUL},rating:{$gt:customerRating},colors:clr,quantity:{$gt:1},RuggedVerrified:rv,influencersChoice:ic,brands:brands,offerApplied:offerSelected});
+            const priceFilter = products.filter((product) => 
+            { 
+                return (product.price > priceLL && product.price < priceUL); 
+            });
+            const customerRatingFilter = products.filter((product)=>{
+                return (product.ratingAvg>customerRating)
+            });
+            const brandFilter = products.filter((product)=>{
+                let index=0;
+                for (index = 0; index < brands.length; index++) {
+                    if (product.brand === brands[index]){
+                        return product.brand===brands[index];
+                    }
+                }
+                return product.brand===brands[index];
+            });
+            const offerFilter = products.filter((product)=>{
+                let index=0;
+                let j=0;
+                for (index = 0; index < offerSelected.length; index++) {
+                    for (j = 0; j < array.length; j++) {   
+                        if (product.offerApplied[j] === offerSelected[index]) {
+                            return product.offerApplied[j] === offerSelected[index];
+                        }
+                    }
+                }
+                return product.offerApplied[j] === offerSelected[index];
+            });
+            const influencersChoiceFilter = products.filter((product)=>{
+                if(ic===true)
+                {
+                    return product.InfluencersChoice.length()>1;
+                }
+                return product;
+            });
+            const ruggedVerrifiedFilter = products.filter((product)=>{
+                if(rv===true)
+                {
+                    return product.RuggedVerrified==="true";
+                }
+            });
+            const colorFilter = products.filter((product)=>{
+                return product.colour===clr;
+            });
+            const availabilityFilter = products.filter((product)=>{
+                return product.quantity>1;
+            });
+            
+            res.render('productSearchPage.ejs', { productsData: products, user: req.session.user, isLoggedin: req.session.isLoggedin, searchTerm: searchTerm });
         })
         .catch(err => {
             console.error(err);
             res.status(500).send('Server Error');
             return;
         });
-    
+
 });
 
-module.exports=router;
+module.exports = router;
