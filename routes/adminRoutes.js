2937dfed27b77f64c60867f543d237ee8517eca3
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2023-04-23 15:13:08 +0530

    admin middleware

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const adminController=require("../controllers/adminController");
 const authMiddleware=require("../middleware/authMiddleware");
+const adminMiddleware=require('../middleware/adminMiddleware');
 const complaintController=require("../controllers/complaintsController");
 const router=express.Router();
 
-router.get('/admin',authMiddleware,adminController.getAdminPage); //need to add isAdmin middleware
+router.get('/admin',adminMiddleware,authMiddleware,adminController.getAdminPage);
 
 module.exports=router;
