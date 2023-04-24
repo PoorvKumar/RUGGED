19cef018467c69fe9cbf64e55e7397ca43864273
Divyank <divyankkhajuria88@gmail.com>
2023-04-24 22:17:42 +0530

    chnages

--- routes/productPage.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/productPage', function(req, res, next) {
-  res.render('productPage',{user:req.session.user , isLoggedin:req.session.isLoggedin,productInfo:product } );
-})
-
-module.exports = router;
